a44a90b21537b1939fec47e4ef2182ffa2c5e53d
Amr Ali <amr.ali.cc@gmail.com>
2013-12-11 12:16:07 +0200

    Update Shannon's function with a more succinct version

--- python/shannon.py
+++ python/shannon.py
@@ -1,12 +1,9 @@
 import math
 from collections import Counter
 
+# http://rosettacode.org/wiki/Entropy#Python:_More_succinct_version
 def H(data):
     if not data: return
-
-    entropy = 0
-    data_len = float(len(data))
-    data_freq = dict(Counter(data)) # Any zero elements are not included
-    p_x = [data_freq[chr(x)] / data_len for x in range(256) if chr(x) in data_freq]
-    return reduce(lambda x, y: x + (- y * math.log(y, 2)), p_x, 0)
+    p, lns = Counter(data), float(len(data))
+    return -sum(count/lns * math.log(count/lns, 2) for count in p.values())
 
